753dceab668be335db600746990a6ddbc31ddd49
André Scholz (sneakyx) <info@rothaarsystems.de>
2015-11-14 12:58:28 +0100

    Version 0.0.1 (pre-alpha) - just empty files

--- ROSInE/setup/setup.inc.php
+++ ROSInE/setup/setup.inc.php
@@ -7,22 +7,30 @@
 	*  This program is free software; you can redistribute it and/or modify it *
 	*  under the terms of the GNU General Public License as published by the   *
 	*  Free Software Foundation; version 2 of the License.                     *
-	*  date of this file: 2015-11-14											*
 	\**************************************************************************/
 
 
-$GLOBALS['phpgw_info']['flags']['currentapp'] = 'invoice';
-include('../header.inc.php');
-// importierung von Stylesheets
-echo ' <link rel="stylesheet" type="text/css" href="invoice.css" media="all">';
+	$setup_info['invoice']['name']	= 'invoice';
+	$setup_info['invoice']['title']	= 'ROSInE';
+	$setup_info['invoice']['version']	= '0.0.1';
+	$setup_info['invoice']['app_order']	= 33;
 
-echo "Willkommen zu ROSInE!<br>";
-// Variablenumwandlung und Deklaration
-// Eigene Verbindung zur Datenbank
-@$db = mysql_connect($egw_info["server"]["db_host"], $egw_info["server"]["db_user"], $egw_info["server"]["db_pass"]) OR die("Fehler mit Datenbank");
-@mysql_select_db($egw_info["server"]["db_name"],$db) OR die ("Falsche Datenbank!");
+	$setup_info['invoice']['author']	= array(
+		'name'	=> 'André Scholz',
+		'email'	=> 'info@rothaarsystems.de',
+	);
+
+	$setup_info['invoice']['license']	= 'GPL';
+	$setup_info['invoice']['description']= 'Help for telephone calls';
+
+	$setup_info['invoice']['maintainer'] = array(
+		'name'	=> 'André Scholz',
+		'email'	=> 'info@rothaarsystems.de',
+	);
+
+
+	$setup_info['invoice']['tables']    = array();
+	$setup_info['invoice']['enable']    = 1;
 
 
 ?>
-
-
