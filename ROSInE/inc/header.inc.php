ca19a900d73c304e3b5f3c99fae8c3fb023cc90b
André Scholz (sneakyx) <info@rothaarsystems.de>
2015-11-22 22:05:21 +0100

    Header test

--- ROSInE/inc/header.inc.php
+++ ROSInE/inc/header.inc.php
@@ -2,12 +2,9 @@
 echo ' <link rel="stylesheet" type="text/css" href="rosine.css" media="all">';
 echo'
 		<div class="rosine_header">
-			<p>
-				<a href="#" class="rosine_header">
-					<img src="templates/default/images/navbar.png" align="middle">
-					<h1 class="rosine_header">ROSInE</h1>
-				</a>
-			</p>
+			<a href="#" class="rosine_header">
+				<h1 class="rosine_header">ROSInE</h1>
+			</a>
 		</div>
 ';
 
