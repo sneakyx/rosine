5fc5c4452972cd62352a7162f848bcdddbf9b1d9
André Scholz (sneakyx) <info@rothaarsystems.de>
2015-11-22 21:51:53 +0100

    Heder Test

--- ROSInE/inc/header.inc.php
+++ ROSInE/inc/header.inc.php
@@ -2,7 +2,7 @@
 echo ' <link rel="stylesheet" type="text/css" href="rosine.css" media="all">';
 echo'
 <div class"rosine_header">
-<a href="#" class="rosine_header"><img src="templates/default/images/navbar.png">
+<a href="#" class="rosine_header"><img src="templates/default/images/navbar.png" align="center">
 <h1 class="rosine_header">ROSInE</h1>
 </a></div>
 ';
