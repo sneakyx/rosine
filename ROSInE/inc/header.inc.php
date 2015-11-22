8a7457c8eeecda12b955353abd1b801efd4ba926
André Scholz (sneakyx) <info@rothaarsystems.de>
2015-11-22 21:28:49 +0100

    Header test

--- ROSInE/inc/header.inc.php
+++ ROSInE/inc/header.inc.php
@@ -1,15 +1,10 @@
 <?
 echo'
 <a href="#">
-<svg viewBox="0 0 100 100" class="icon">
-<use xlink:href="#leaf">
-<rect x="20" y="400" width="150" rx="100" ry="100" height="75"
-30:     style="fill: #FEE; stroke: #000; stroke-width: 2px"/>
 <div>
 ROSInE
 <span></span>
 </div>
-</svg>
 </a>
 ';
 
