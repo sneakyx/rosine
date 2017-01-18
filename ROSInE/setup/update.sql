11441e63cbe048d1cbe4d4b48cf9e4baf4a940bd
André Scholz (sneakyx) <info@rothaarsystems.de>
2017-01-18 18:08:17 +0100

    new css file- better usability

---
+++ ROSInE/setup/update.sql
@@ -0,0 +1,18 @@
+# FROM 2016-12-30 to 2017-01-02
+
+INSERT INTO `rosine_config` (`config`, `user_id`, `value`) VALUES
+('overwrite_templates', 0, 'no');
+
+# FROM 2017-01-02 to 2017-01-14
+
+INSERT INTO `egroupware`.`rosine_notes` (`NOTE_ID` , `LANGUAGE` , `NOTE_TEXT`) VALUES
+ ( '100', 'de.php', '%SINGULAR% %ID% vom %date%:'), 
+('100', 'en.php', '%SINGULAR% %ID% from %date%:');
+
+INSERT INTO `egroupware`.`rosine_config` (`config` ,`user_id` ,`value`) VALUES 
+('insert_delivery_into_paperwork', '0', '100'), 
+('insert_offer_into_paperwork', '0', '100'),
+('insert_order_into_paperwork', '0', '100'), 
+('insert_invoice_into_paperwork', '0', '100');
+
+
